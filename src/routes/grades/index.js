00b9eb85cbafa65077e17891d7144647550a7ff1
FatBanana <34758569+refact0r@users.noreply.github.com>
2022-01-05 17:36:24 -0800

    fix

--- src/routes/grades/index.js
+++ src/routes/grades/index.js
@@ -1,13 +1,15 @@
 import studentvue from 'studentvue.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
-let username = 's-weny';
-let password = 'ilikepie123();';
+let username = process.env['USER'];
+let password = process.env['PASSWORD'];
 
 export async function get() {
+	console.log(username, password);
 	let client = await studentvue.login('https://wa-bsd405-psv.edupoint.com/', username, password);
 	let gradebook = JSON.parse(await client.getGradebook(username, password));
 	return {
-		status: 200,
 		body: gradebook.Gradebook
 	};
 }
