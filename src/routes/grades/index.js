46af46cb6604845b46c85b85723d497559a0f2e0
FatBanana <34758569+refact0r@users.noreply.github.com>
2022-01-05 21:05:09 -0800

    test

--- src/routes/grades/index.js
+++ src/routes/grades/index.js
@@ -5,7 +5,9 @@ dotenv.config();
 export async function get() {
 	const user = process.env['USER'];
 	const password = process.env['PASSWORD'];
+	console.log(user, password);
 	let client = await studentvue.login('https://wa-bsd405-psv.edupoint.com/', user, password);
+	console.log(client);
 	let test = await client.getGradebook(user, password);
 	console.log(test);
 	let gradebook = JSON.parse(test);
