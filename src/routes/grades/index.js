bff2d3c37616184807ecc668df93a7fc34d17652
refact0r <34758569+refact0r@users.noreply.github.com>
2022-01-06 11:12:42 -0800

    Update index.js

--- src/routes/grades/index.js
+++ src/routes/grades/index.js
@@ -2,16 +2,14 @@ import { login } from 'studentvue.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export async function get() {
-	const user = process.env['USER'];
-	const password = process.env['PASSWORD'];
-	console.log(user, password);
-	console.log(login);
-	let client = await login('https://wa-bsd405-psv.edupoint.com/', user, password);
-	console.log(client);
-	let test = await client.getGradebook(user, password);
-	console.log(test);
-	let gradebook = JSON.parse(test);
+const username = process.env['USER'];
+const password = process.env['PASSWORD'];
+
+export async function get(request) {
+	const str = Buffer.from(req.headers.authorization.split(" ")[1], 'base64').toString()
+	console.log(str);
+	let client = await login('https://wa-bsd405-psv.edupoint.com/', username, password);
+	let gradebook = JSON.parse(await client.getGradebook(username, password));
 	return {
 		body: gradebook.Gradebook
 	};
