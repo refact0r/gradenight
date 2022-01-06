2013624867803e9317b5fee65b8211edf7f51a34
FatBanana <yifanw06@gmail.com>
2022-01-05 21:55:18 -0800

    test

--- src/routes/grades/index.js
+++ src/routes/grades/index.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
-const studentvue = require('studentvue.js');
 
 export async function get() {
+	const studentvue = require('studentvue.js');
 	const user = process.env['USER'];
 	const password = process.env['PASSWORD'];
 	console.log(user, password);
