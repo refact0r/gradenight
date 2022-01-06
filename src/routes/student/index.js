aa72bc01a3f2e74838abc67ef123fe60309108bb
FatBanana <34758569+refact0r@users.noreply.github.com>
2022-01-05 17:17:15 -0800

    test

--- src/routes/student/index.js
+++ src/routes/student/index.js
@@ -6,13 +6,10 @@ let username = process.env['USERNAME'];
 let password = process.env['PASSWORD'];
 
 export async function get() {
-	if (!username || !password) {
-		return {
-			status: 511
-		};
-	}
 	let client = await studentvue.login('https://wa-bsd405-psv.edupoint.com/', username, password);
-	let gradebook = JSON.parse(await client.getGradebook(username, password));
+	let test = await client.getGradebook(username, password);
+	console.log(test);
+	let gradebook = JSON.parse(test);
 	return {
 		status: 200,
 		body: gradebook.Gradebook
