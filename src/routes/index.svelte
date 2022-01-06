4dab750ae50a3884367816f70df2defd811b49a9
refact0r <34758569+refact0r@users.noreply.github.com>
2022-01-06 10:55:31 -0800

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script>
 	let grades;
+	let username;
+	let password;
 	async function getGrades() {
 		let res = await fetch('/grades');
 		grades = await res.json();
@@ -13,6 +15,10 @@
 
 {#if grades}
 	<h3>{grades.ReportingPeriod.GradePeriod}</h3>
+	Username<br/>
+	<input type="text" bind:value={username}/>
+	Password<br/>	
+	<input type="password" bind:value={password}/>
 	<table>
 		{#each grades.Courses.Course as course}
 			<tr>
