9774016c84bf52165cdc9858e6aa6a0e3804d57c
refact0r <34758569+refact0r@users.noreply.github.com>
2022-01-07 11:43:56 -0800

    Update __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,23 @@
 <script context="module">
 	export async function load({ page }) {
+		const auth = localStorage.get('authorization')
+		let grades;
+		if (auth) {
+			let res = await fetch('/grades', {
+				headers: { Authorization: 'Basic ' + btoa(username + ':' + password) }
+			});
+			grades = await res.json();
+		}
 		return {
 			props: {
-				key: page.path
+				grades
 			}
 		}
 	}
 </script>
 
 <script>
-    
+    export let grades;
 </script>
 
-<slot />
+<slot {grades}/>
