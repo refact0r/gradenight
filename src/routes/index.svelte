aab0f97981cbb137d0dafd38a157d842f85f265f
FatBanana <34758569+refact0r@users.noreply.github.com>
2022-01-06 18:39:51 -0800

    use form

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
 	let password;
 	async function getGrades() {
 		let res = await fetch('/grades', {
-			headers: {'Authorization': 'Basic ' + btoa(username + ':' + password) } 
+			headers: { Authorization: 'Basic ' + btoa(username + ':' + password) }
 		});
 		grades = await res.json();
 		console.log(grades);
@@ -13,11 +13,13 @@
 
 <h1>Synergy Test</h1>
 
-Username <input type="text" bind:value={username}/><br/>
-<br/>
-Password <input type="password" bind:value={password}/><br/>
-<br/>
-<button on:click={getGrades}>Get Grades</button>
+<form on:submit|preventDefault={getGrades}>
+	Username <input type="text" bind:value={username} /><br />
+	<br />
+	Password <input type="password" bind:value={password} /><br />
+	<br />
+	<button type="submit">Get Grades</button>
+</form>
 
 {#if grades}
 	<h3>{grades.ReportingPeriod.GradePeriod}</h3>
