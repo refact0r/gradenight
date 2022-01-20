b70db845ffe0ba48ba138ceb3c5e3ce808c1d2e7
FatBanana <34758569+refact0r@users.noreply.github.com>
2022-01-19 17:39:07 -0800

    test loading bar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,16 +8,21 @@
 	import '../app.css'
 	import { session } from '$app/stores'
 	import { onMount } from 'svelte'
+	import ProgressBar from 'svelte-progress-bar'
+	let progress
 
 	onMount(async () => {
 		if ($session.user) {
 			console.log('fetch')
+			progress.start()
 			const res = await fetch('/all')
 			const json = await res.json()
 			$session.student = json.student
 			$session.gradebook = json.gradebook
+			progress.complete()
 		}
 	})
 </script>
 
+<ProgressBar bind:this={progress} color="#0366d6" intervalTime="100" stepSizes={[0.05]} />
 <slot />
