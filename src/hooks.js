f2335e7fa64ac76c0b27a70e3654ad2d8ff994d5
FatBanana <34758569+refact0r@users.noreply.github.com>
2022-01-25 16:04:56 -0800

    update to new sveltekit

--- src/hooks.js
+++ src/hooks.js
@@ -1,28 +1,23 @@
 import cookie from 'cookie'
 
-export const handle = async ({ request, resolve }) => {
-	const cookies = cookie.parse(request.headers.cookie || '')
+export const handle = async ({ event, resolve }) => {
+	const cookies = cookie.parse(event.request.headers.get('cookie') || '')
 
 	if (cookies.auth) {
 		const auth = cookies.auth.split(':')
-		request.locals.user = {
+		event.locals.user = {
 			username: auth[0],
 			password: auth[1]
 		}
 	}
 
-	const response = await resolve(request)
+	const response = await resolve(event)
 
-	return {
-		...response,
-		headers: {
-			...response.headers
-		}
-	}
+	return response
 }
 
 // Sets session on client-side
 // try console logging session in routes' load({ session }) functions
-export const getSession = async (request) => {
-	return request.locals
+export const getSession = async (event) => {
+	return event.locals
 }
