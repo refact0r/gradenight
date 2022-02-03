bee06a0bd4552c32143f6814d2f2d56329fab6df
refact0r <34758569+refact0r@users.noreply.github.com>
2022-02-03 09:25:39 -0800

    Update login.js

--- src/routes/auth/login.js
+++ src/routes/auth/login.js
@@ -3,17 +3,16 @@ import cookie from 'cookie'
 
 export async function post({ request }) {
 	const body = await request.json()
+	console.log(body)
 
-	let result
 	let client = await login('https://wa-bsd405-psv.edupoint.com/', body.username, body.password)
-	result = await Promise.all([
+	let result = await Promise.all([
 		client.getStudentInfo().then((value) => JSON.parse(value).StudentInfo),
 		client.getGradebook(0).then((value) => JSON.parse(value).Gradebook),
 		client.getGradebook(1).then((value) => JSON.parse(value).Gradebook),
 		client.getGradebook(2).then((value) => JSON.parse(value).Gradebook),
 		client.getGradebook(3).then((value) => JSON.parse(value).Gradebook)
 	])
-	console.log(result)
 	if (!result[0]) {
 		return {
 			status: 401
