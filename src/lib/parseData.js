695fd51afa8fc39e056c5f848c12c3d4e7a72b74
FatBanana <yifanw06@gmail.com>
2022-02-24 17:05:20 -0800

    refactor parseData and fix graph dates

--- src/lib/parseData.js
+++ src/lib/parseData.js
@@ -6,8 +6,9 @@ export function parseData(session, json) {
 
 	for (let period of periods) {
 		let grades = []
+		let assignments = []
 
-		for (let course of period.Courses.Course) {
+		for (let [index, course] of period.Courses.Course.entries()) {
 			course.Title = course.Title.replace(/ \([\s\S]*?\)/g, '')
 			course.fourPoint = false
 
@@ -35,15 +36,122 @@ export function parseData(session, json) {
 
 			course.color = getColor(course.scorePercent)
 			course.style = `color: ${course.color};`
+			course.chartData = []
+
+			if (!course.Marks.Mark.Assignments.Assignment) {
+				continue
+			}
+
+			if (!Array.isArray(course.Marks.Mark.Assignments.Assignment)) {
+				course.Marks.Mark.Assignments.Assignment = [
+					course.Marks.Mark.Assignments.Assignment
+				]
+			}
+
+			let weighted = false
+			let scoreTypes = {}
+
+			if (course.Marks.Mark.GradeCalculationSummary.AssignmentGradeCalc) {
+				weighted = true
+
+				for (let type of course.Marks.Mark.GradeCalculationSummary.AssignmentGradeCalc) {
+					if (parseInt(type.Weight) !== 100.0) {
+						scoreTypes[type.Type] = {
+							score: 0,
+							total: 0,
+							weight: parseInt(type.Weight)
+						}
+					}
+				}
+			} else {
+				scoreTypes.all = {
+					score: 0,
+					total: 0,
+					weight: 100
+				}
+			}
+
+			for (let assignment of course.Marks.Mark.Assignments.Assignment.reverse()) {
+				assignment.course = course.Title
+				assignment.courseIndex = index
+				assignment.style = null
+				assignment.scorePercent = -1
+				assignment.percent = '?'
+				assignment.score = 'Not Graded'
+				assignment.Measure = assignment.Measure.replace('&amp;', '&')
+
+				if (assignment.Points.includes(' / ')) {
+					let split = assignment.Points.split(' / ')
+					let scoreValue = parseFloat(split[0])
+					let totalValue = parseFloat(split[1])
+					assignment.score = scoreValue + ' / ' + totalValue
+
+					if (
+						(scoreValue === 0 && totalValue === 0) ||
+						assignment.Notes.toLowerCase().includes('not for grading')
+					) {
+						assignment.scorePercent = 0
+						assignment.percent = '-'
+					} else {
+						assignment.scorePercent = (scoreValue / totalValue) * 100
+						assignment.percent = assignment.scorePercent
+							? assignment.scorePercent.toFixed(1) + '%'
+							: '0.0%'
+						assignment.style = `color: ${getColor(assignment.scorePercent)};`
+
+						if (weighted) {
+							if (scoreTypes[assignment.Type]) {
+								scoreTypes[assignment.Type].score += scoreValue
+								scoreTypes[assignment.Type].total += totalValue
+							}
+						} else {
+							scoreTypes.all.score += scoreValue
+							scoreTypes.all.total += totalValue
+						}
+
+						let date = new Date(assignment.DueDate)
+						let score = 0
+						let total = 0
+
+						for (let type of Object.values(scoreTypes)) {
+							if (type.total > 0) {
+								score += (type.score / type.total) * type.weight
+								total += type.weight
+							}
+						}
+
+						let color = getColor((score / total) * 100)
+						let grade = (score / total) * (course.fourPoint ? 4 : 100)
+
+						if (
+							course.chartData.length > 0 &&
+							course.chartData[course.chartData.length - 1].x ===
+								Math.floor(date / 8.64e7)
+						) {
+							course.chartData[course.chartData.length - 1].y = grade
+							course.chartData[course.chartData.length - 1].color = color
+						} else {
+							course.chartData.push({
+								x: Math.floor(date / 8.64e7),
+								y: grade,
+								color: color
+							})
+						}
+					}
+				}
+				assignments.push(assignment)
+			}
 		}
 
 		let averageRaw = -1
 		if (grades.length > 0) averageRaw = grades.reduce((a, b) => a + b) / grades.length
+
 		period.averageStyle = `color: ${getColor(averageRaw)};`
 		period.average = averageRaw >= 0 ? averageRaw.toFixed(1) + '%' : '-'
 
 		period.days = Math.round((new Date(period.ReportingPeriod.EndDate) - new Date()) / 86400000)
-		period.assignments = getAssignments(period)
+		assignments.sort((a, b) => new Date(b.DueDate) - new Date(a.DueDate))
+		period.assignments = assignments
 		period.week = getWeek(period.assignments)
 	}
 
@@ -56,110 +164,6 @@ export function parseData(session, json) {
 		selected: periods[currentPeriod],
 		gradebook: periods[currentPeriod]
 	}
-}
-
-function getAssignments(gradebook) {
-	let assignments = []
-
-	for (const [index, course] of gradebook.Courses.Course.entries()) {
-		course.chartData = []
-		if (!course.Marks.Mark.Assignments.Assignment) {
-			continue
-		}
-		if (!Array.isArray(course.Marks.Mark.Assignments.Assignment)) {
-			course.Marks.Mark.Assignments.Assignment = [course.Marks.Mark.Assignments.Assignment]
-		}
-		let weighted = false
-		let scoreTypes = {}
-		if (course.Marks.Mark.GradeCalculationSummary.AssignmentGradeCalc) {
-			weighted = true
-			for (let type of course.Marks.Mark.GradeCalculationSummary.AssignmentGradeCalc) {
-				if (parseInt(type.Weight) !== 100.0) {
-					scoreTypes[type.Type] = {
-						score: 0,
-						total: 0,
-						weight: parseInt(type.Weight)
-					}
-				}
-			}
-		} else {
-			scoreTypes.all = {
-				score: 0,
-				total: 0,
-				weight: 100
-			}
-		}
-
-		for (let assignment of course.Marks.Mark.Assignments.Assignment.reverse()) {
-			assignment.course = course.Title
-			assignment.courseIndex = index
-			assignment.style = null
-			assignment.scorePercent = -1
-			assignment.percent = '?'
-			assignment.score = 'Not Graded'
-			assignment.Measure = assignment.Measure.replace('&amp;', '&')
-
-			if (assignment.Points.includes(' / ')) {
-				let split = assignment.Points.split(' / ')
-				let scoreValue = parseFloat(split[0])
-				let totalValue = parseFloat(split[1])
-				assignment.score = scoreValue + ' / ' + totalValue
-
-				if (
-					(scoreValue === 0 && totalValue === 0) ||
-					assignment.Notes.toLowerCase().includes('not for grading')
-				) {
-					assignment.scorePercent = 0
-					assignment.percent = '-'
-				} else {
-					assignment.scorePercent = (scoreValue / totalValue) * 100
-					assignment.percent = assignment.scorePercent
-						? assignment.scorePercent.toFixed(1) + '%'
-						: '0.0%'
-					assignment.style = `color: ${getColor(assignment.scorePercent)};`
-					if (weighted) {
-						if (scoreTypes[assignment.Type]) {
-							scoreTypes[assignment.Type].score += scoreValue
-							scoreTypes[assignment.Type].total += totalValue
-						}
-					} else {
-						scoreTypes.all.score += scoreValue
-						scoreTypes.all.total += totalValue
-					}
-					let date = new Date(assignment.DueDate)
-					let score = 0
-					let total = 0
-					for (let type of Object.values(scoreTypes)) {
-						if (type.total > 0) {
-							score += (type.score / type.total) * type.weight
-							total += type.weight
-						}
-					}
-					let color = getColor((score / total) * 100)
-					let grade = (score / total) * (course.fourPoint ? 4 : 100)
-
-					if (
-						course.chartData.length > 0 &&
-						course.chartData[course.chartData.length - 1].x ===
-							Math.floor(date / 8.64e7)
-					) {
-						course.chartData[course.chartData.length - 1].y = grade
-						course.chartData[course.chartData.length - 1].color = color
-					} else {
-						course.chartData.push({
-							x: Math.floor(date / 8.64e7),
-							y: grade,
-							color: color
-						})
-					}
-				}
-			}
-			assignments.push(assignment)
-		}
-	}
-
-	assignments.sort((a, b) => new Date(b.DueDate) - new Date(a.DueDate))
-	return assignments
 }
 
 function getWeek(assignments) {
