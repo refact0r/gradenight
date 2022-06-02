4dd484af4d1e89c68ca0589d6816fcab32de5145
FatBanana <yifanw06@gmail.com>
2022-06-01 17:47:45 -0700

    css fixes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -123,6 +123,12 @@
 {/if}
 
 <style lang="scss">
+	@media (max-width: $breakpoint-phone) {
+		.content {
+			background: var(--bg-color-2);
+		}
+	}
+
 	nav {
 		@include box;
 		display: flex;
@@ -203,14 +209,15 @@
 		background: transparent;
 		&:hover {
 			background: var(--bg-color-1-5);
-			// color: var(--font-color-2);
 		}
 		&:active {
 			background: var(--bg-color-1);
-			// color: var(--font-color-3);
 			i {
 				transform: scale(0.9);
 			}
+		}
+		&.active {
+			background: var(--bg-color-1);
 		}
 	}
 
