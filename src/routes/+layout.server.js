6dc77fbf158ff5d70675c909ebbcf951d2a4a7dd
refact0r <34758569+refact0r@users.noreply.github.com>
2022-11-14 22:03:32 +0000

    add login redirect

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,7 +1,5 @@
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params, locals }) {
-	console.log('layout server load')
-	return {
-		user: locals
-	}
+	console.log(locals)
+	return locals
 }
