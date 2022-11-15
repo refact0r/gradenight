46c6aa6f0e4f7303410d39728550a6214be4f5ec
refact0r <34758569+refact0r@users.noreply.github.com>
2022-11-14 17:11:00 -0800

    response for invalid fetch data

--- src/routes/data/+server.js
+++ src/routes/data/+server.js
@@ -5,23 +5,28 @@ export async function GET({ locals }) {
 	console.log('get data')
 	console.log(locals)
 
-	let client = await login(
-		Buffer.from(locals.user.districtUrl, 'base64').toString('ascii'),
-		Buffer.from(locals.user.username, 'base64').toString('ascii'),
-		Buffer.from(locals.user.password, 'base64').toString('ascii')
-	)
-	// let student = JSON.parse(await client.getStudentInfo()).StudentInfo
-	// let gradebook = JSON.parse(await client.getGradebook()).Gradebook
-	const result = await Promise.all([
-		client.getStudentInfo().then((value) => JSON.parse(value).StudentInfo),
-		client.getGradebook(0).then((value) => JSON.parse(value).Gradebook),
-		client.getGradebook(1).then((value) => JSON.parse(value).Gradebook),
-		client.getGradebook(2).then((value) => JSON.parse(value).Gradebook),
-		client.getGradebook(3).then((value) => JSON.parse(value).Gradebook)
-	])
+	try {
+		let client = await login(
+			Buffer.from(locals.user.districtUrl, 'base64').toString('ascii'),
+			Buffer.from(locals.user.username, 'base64').toString('ascii'),
+			Buffer.from(locals.user.password, 'base64').toString('ascii')
+		)
+		// let student = JSON.parse(await client.getStudentInfo()).StudentInfo
+		// let gradebook = JSON.parse(await client.getGradebook()).Gradebook
+		const result = await Promise.all([
+			client.getStudentInfo().then((value) => JSON.parse(value).StudentInfo),
+			client.getGradebook(0).then((value) => JSON.parse(value).Gradebook),
+			client.getGradebook(1).then((value) => JSON.parse(value).Gradebook),
+			client.getGradebook(2).then((value) => JSON.parse(value).Gradebook),
+			client.getGradebook(3).then((value) => JSON.parse(value).Gradebook)
+		])
 
-	if (!result[0]) {
-		return {
+		if (!result[0]) {
+			throw new Error('No data returned')
+		}
+	} catch (error) {
+		return new Response(null, {
+			status: 401,
 			headers: {
 				'Set-cookie': cookie.serialize('auth', '', {
 					httpOnly: true,
@@ -30,7 +35,7 @@ export async function GET({ locals }) {
 					expires: new Date(0)
 				})
 			}
-		}
+		})
 	}
 
 	console.log('logged in')
