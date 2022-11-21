8d68e6efd497d95cbed53ce5ec4f16a6a9788206
refact0r <34758569+refact0r@users.noreply.github.com>
2022-11-21 18:10:17 +0000

    fix codes

--- src/routes/course/[courseIndex]/+page.server.js
+++ src/routes/course/[courseIndex]/+page.server.js
@@ -5,7 +5,7 @@ export async function load({ params, locals }) {
 	console.log('course server load')
 	if (!locals.user) {
 		console.log('redirect to login')
-		throw redirect(401, '/login')
+		throw redirect(302, '/login')
 	}
 	return {
 		courseIndex: params.courseIndex
