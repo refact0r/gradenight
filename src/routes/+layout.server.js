8f3d72b3419f882bf0b94280bdd7167299395368
refact0r <34758569+refact0r@users.noreply.github.com>
2023-02-04 12:40:08 -0800

    load data on pageload

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,5 +1,18 @@
+import { redirect } from '@sveltejs/kit'
+
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ locals, url }) {
+export async function load({ fetch, locals, url }) {
 	console.log('layout server load')
-	return { ...locals, url: url.href }
+
+	let json
+	if (locals.user) {
+		const res = await fetch('/data')
+		if (!res.ok) {
+			console.log('fetch data not ok: ', res.status)
+			throw redirect(302, '/login')
+		}
+		json = await res.json()
+	}
+
+	return { ...json, ...locals, url: url.href }
 }
