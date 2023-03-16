4889afaeed9213a32787afc8f45be505bb503627
yifan <34758569+refact0r@users.noreply.github.com>
2023-03-16 13:37:01 -0700

    fix d grade value

--- src/lib/js/utils.js
+++ src/lib/js/utils.js
@@ -24,7 +24,7 @@ export function percentToLetter(grade) {
 	if (grade >= 90) return 'A'
 	else if (grade >= 80) return 'B'
 	else if (grade >= 70) return 'C'
-	else if (grade === 60) return 'D'
+	else if (grade >= 60) return 'D'
 	else return 'F'
 }
 
